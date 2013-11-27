5a444137ffb7c0485cd88a9408e769f77f6fce59
Jorge Luis Hernández C <lesthack@gmail.com>
2013-11-27 14:03:12 -0600

    Admin site django tuned

--- web/admin.py
+++ web/admin.py
@@ -2,5 +2,18 @@
 from django.contrib import admin
 from web.models import *
 
-admin.site.register(category)
-admin.site.register(item)
+class itemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'username', 'never_expire', 'expire_at', 'created_at', 'created_by']
+    list_display_links = ['id','name']
+    search_fields = ['name', 'category__name', 'url', 'username', 'password', 'comments', 'created_by__username']
+    list_filter = ['category', 'expire_at', 'created_at']
+
+class categoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_at','created_by', 'updated_at']
+    list_display_links = ['id', 'name']
+    search_fields = ['name', 'created_by__username']
+    list_filter = ['created_by', 'created_at']
+
+#admin.site.unregister(site)
+admin.site.register(category, categoryAdmin)
+admin.site.register(item, itemAdmin)
